25a89ca3efef52bdcaab59e7943d5908c3b16026
FarhanVirenze <farhanvirenze18@gmail.com>
2023-04-13 14:38:58 +0700

    tambahan fungsi if dan else

--- LinearSearch_139/LinearSearch_139.cpp
+++ LinearSearch_139/LinearSearch_139.cpp
@@ -13,13 +13,19 @@ void input()
 {
     while (true)
     {
-
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
 }
 
+
 int main()
 {
-    std::cout << "Hello World!\n";
+   
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
